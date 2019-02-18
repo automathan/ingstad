b1e97374dc44072b2484ac8a32da665b910747a8
Jontahan <jona.jorg@gmail.com>
2019-02-18 22:10:09 +0100

    Setup a rough RL system

--- boat_env.py
+++ boat_env.py
@@ -11,7 +11,7 @@ excl_mark, _ = font.render('!!!', (255, 0, 0))
 
 class Boat:
     NOP, TURN_CW, TURN_CCW, INC_SPD, DEC_SPD = range(5)
-    def __init__(self, pos, env, initial_speed=0, initial_acceleration=0, initial_direction=0, initial_angular_velocity=0):
+    def __init__(self, pos, env, initial_speed=0, initial_acceleration=0, initial_direction=0, initial_angular_velocity=0, wrap=True):
         self.x = pos[0]
         self.y = pos[1]
         self.length = 96
@@ -23,46 +23,59 @@ class Boat:
         self.other_ship_dist = -1
         self.max_ship_spd = 10
         self.min_ship_spd = -5
+        self.show_circles = False
+        self.goal_position = (0, 0)
+        self.state = [0.0, 0.0, 0.0, 0.0]
+        self.wrap = wrap
 
     def step(self, action):
-        if action == Boat.INC_SPD:
-            self.speed += 0.1
-        if action == Boat.DEC_SPD:
-            self.speed -= 0.1
-        if action == Boat.TURN_CCW:
-            self.angular_velocity += 0.1
-        if action == Boat.TURN_CW:
-            self.angular_velocity -= 0.1
+        done = False
+
+        if action == Boat.INC_SPD:  self.speed += 0.1
+        if action == Boat.DEC_SPD:  self.speed -= 0.1
+        if action == Boat.TURN_CCW: self.angular_velocity += 0.1
+        if action == Boat.TURN_CW:  self.angular_velocity -= 0.1
 
         self.x += self.speed * math.cos(math.radians(self.direction))
         self.y += self.speed * -math.sin(math.radians(self.direction))
-
-        if self.x < 0: self.x = self.env.dimensions[0]
-        if self.y < 0: self.y = self.env.dimensions[1]
-        if self.x > self.env.dimensions[0]: self.x = 0
-        if self.y > self.env.dimensions[1]: self.y = 0
-
+        
+        if self.wrap:
+            if self.x < 0: self.x = self.env.dimensions[0]
+            if self.y < 0: self.y = self.env.dimensions[1]
+            if self.x > self.env.dimensions[0]: self.x = 0
+            if self.y > self.env.dimensions[1]: self.y = 0
+        else:
+            if self.x < 0: done = True
+            if self.y < 0: done = True
+            if self.x > self.env.dimensions[0]: done = True
+            if self.y > self.env.dimensions[1]: done = True
+        
         self.speed += self.acceleration
         self.direction += self.angular_velocity
         
         self.other_ship_dist = math.sqrt((self.env.boat1.x - self.env.boat2.x) ** 2 + (self.env.boat1.y - self.env.boat2.y) ** 2)
+        dist_to_goal = math.sqrt((self.x - self.goal_position[0]) ** 2 + (self.y - self.goal_position[1]) ** 2)
         
-        state = [self.x / self.env.dimensions[0], self.y / self.env.dimensions[1]]
-        reward = 0
-        done = False
+        self.state = [self.x / self.env.dimensions[0], self.y / self.env.dimensions[1], 0.0, 0.0]
+        reward = -1
+        
+        if dist_to_goal < 200:
+            reward = 10
 
-        return state, reward, done, {}
+        return self.state, reward, done, {}
 
     def draw(self, screen):
         boat_image = pg.transform.rotate(myimage, self.direction)
         b_img_w, b_img_h = (boat_image.get_width(), boat_image.get_height())
         boat_pivot = (self.x - b_img_w / 2, self.y - b_img_h / 2)
         screen.blit(boat_image, imagerect.move(boat_pivot))
-        pg.draw.circle(screen, (255, 0, 0), (int(self.x), int(self.y)), 4)
-        pg.draw.circle(screen, (255, 0, 0), (int(self.x), int(self.y)), self.length // 2, 1)
-        pg.draw.circle(screen, (255, 140, 0), (int(self.x), int(self.y)), self.length, 1)
-        pg.draw.circle(screen, (255, 187, 0), (int(self.x), int(self.y)), self.length * 2, 1)
-        pg.draw.circle(screen, (255, 250, 0), (int(self.x), int(self.y)), self.length * 4, 1)
+        
+        if self.show_circles:
+            pg.draw.circle(screen, (255, 0, 0), (int(self.x), int(self.y)), 4)
+            pg.draw.circle(screen, (255, 0, 0), (int(self.x), int(self.y)), self.length // 2, 1)
+            pg.draw.circle(screen, (255, 140, 0), (int(self.x), int(self.y)), self.length, 1)
+            pg.draw.circle(screen, (255, 187, 0), (int(self.x), int(self.y)), self.length * 2, 1)
+            pg.draw.circle(screen, (255, 250, 0), (int(self.x), int(self.y)), self.length * 4, 1)
         
         # line to other ship
         pg.draw.line(screen, (255, 0, 0), (self.env.boat1.x, self.env.boat1.y), (self.env.boat2.x, self.env.boat2.y))
@@ -70,21 +83,39 @@ class Boat:
         # forward line
         pg.draw.line(screen, (255, 0, 0), (self.x, self.y), (self.x + self.length * math.cos(math.radians(self.direction)), self.y + self.length * -math.sin(math.radians(self.direction))))
         
+        # line to goal position
+        pg.draw.line(screen, (255, 0, 0), (self.x, self.y), self.goal_position)
+        
         if self.other_ship_dist < self.length * 4:
             screen.blit(excl_mark, (self.x - 10, self.y - 40))                
 
+class ActionSpace(list):
+    n = property(lambda self : len(self))
+
 class BoatEnvironment:
-    def __init__(self, dimensions):
-        self.boat1 = Boat((dimensions[0] // 2, dimensions[1] // 2), self, initial_speed=2, initial_angular_velocity=1)
+    def __init__(self, dimensions, screen):
+        self.boat1 = Boat((dimensions[0] // 2, dimensions[1] // 2), self, initial_speed=2, initial_angular_velocity=1, wrap=False)
         self.boat2 = Boat((dimensions[0] // 2, dimensions[1] // 2), self, initial_speed=2, initial_angular_velocity=-1)
         self.dimensions = dimensions
-        self.action_space = [
+        self.action_space = ActionSpace([
             Boat.NOP,
             Boat.TURN_CW,
             Boat.TURN_CCW,
             Boat.INC_SPD,
             Boat.DEC_SPD
-        ]    
+        ])
+        self.observation_space = np.ndarray((4))
+        self.screen = screen
+
+    def step(self, action):
+        return self.boat1.step(action)
+
+    def reset(self):
+        self.boat1 = Boat((self.dimensions[0] // 2, self.dimensions[1] // 2), self, initial_speed=2, initial_angular_velocity=1)
+        return self.boat1.state
+
+    def render(self):
+        self.draw(self.screen)
 
     def draw(self, screen):
         # water
--- main.py
+++ main.py
@@ -2,23 +2,41 @@ from boat_env import BoatEnvironment, Boat
 import pygame as pg
 import numpy as np
 from vicero.policy import RandomPolicy, KeyboardPolicy
+from vicero.algorithms.deepqlearning import DQN
+from vicero.algorithms.common.neuralnetwork import NetworkSpecification
 
-env = BoatEnvironment((960, 960))
+
+dim = (960, 960)
 
 pg.init()
-screen = pg.display.set_mode(env.dimensions)
+screen = pg.display.set_mode(dim)
 clock = pg.time.Clock()
+
+env = BoatEnvironment(dim, screen)
 
 framerate = 30
 
 running = True
 
-boat2_policy = RandomPolicy(env.action_space)
-boat2_state = [0, 0]
-
-boat1_policy = KeyboardPolicy(Boat.NOP, up=Boat.INC_SPD, down=Boat.DEC_SPD, left=Boat.TURN_CCW, right=Boat.TURN_CW)
+boat1_policy = RandomPolicy(env.action_space)
 boat1_state = [0, 0]
 
+boat2_policy = KeyboardPolicy(Boat.NOP, up=Boat.INC_SPD, down=Boat.DEC_SPD, left=Boat.TURN_CCW, right=Boat.TURN_CW)
+boat2_state = [0, 0]
+
+spec = NetworkSpecification()
+dqn = DQN(env, spec, render=True)
+
+batch_size = 32
+num_episodes = 5
+training_iter = 5000
+completion_reward = 5
+
+print('training...')
+dqn.train(num_episodes, batch_size, training_iter, verbose=True, completion_reward=completion_reward, plot=True, eps_decay=True)
+boat1_policy = dqn.copy_target_policy()
+
+state = env.reset()
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -27,8 +45,9 @@ while running:
         
     keys = pg.key.get_pressed() 
     
-    boat1_state, _, _, _ = env.boat1.step(boat1_policy(keys))
-    boat2_state, _, _, _ = env.boat2.step(boat2_policy(boat2_state))
+    state, _, _, _ = env.boat1.step(boat1_policy(state))
+    
+    boat2_state, _, _, _ = env.boat2.step(boat2_policy(keys))
     
     env.draw(screen)
 
